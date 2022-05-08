65d7898893b3d07543a2c803f06529d658343d84
wyf32268886 <1520246115@qq.comxo>
2022-05-09 00:44:41 +0800

    feat: 搜索结果页

---
+++ src/pages/articles/searchResult/index.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @version: 1.0
+ * @Author: 赵卓轩
+ * @Date: 2022-05-09 00:42:37
+ * @LastEditors: 赵卓轩
+ * @LastEditTime: 2022-05-09 00:44:28
+ */
+import React from "react";
+
+const SearchResult: React.FC<{}> = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SearchResult;
